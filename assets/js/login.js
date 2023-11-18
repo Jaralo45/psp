5b5db0b673a8ca1d7a9721338a8a7f8fce725f8b
Jaralo45 <70823260+Jaralo45@users.noreply.github.com>
2023-11-17 21:19:39 -0500

    login-contacto
    Actualización de login y contacto. Implementación de mensajes en alert de bootstrap.

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,32 +6,121 @@ const $submit = document.getElementById("submit"),
       $reserva = document.getElementById("reserva");
       $fechai = document.getElementById("fechai");
       $fechaf = document.getElementById("fechaf");
+      $nombre = document.getElementById("nombre");
+      $correo = document.getElementById("correo");
+      $contacto = document.getElementById("contacto");
 
-document.addEventListener("change", (e)=>{
-    if(e.target === $visible){
-        if($visible.checked === false) $password.type = "password";
-        else $password.type = "text";
-    }
-});
-
-document.addEventListener("click", (e)=>{
-    if(e.target === $submit){
-        if($password.value == "123456" && $username.value == "grupo16"){
+      document.addEventListener("change", (e) => {
+        if (e.target === $visible) {
+            $password.type = $visible.checked ? "text" : "password";
+        }
+    });
+    
+    document.addEventListener("click", (e) => {
+        if (e.target === $submit) {
             e.preventDefault();
-            window.location.href = "index.html";
-        }else{
-            alert("Usuario o contraseña incorrecta");
+    
+            if ($password.value === "123456" && $username.value === "grupo16") {
+                const alertElement = document.createElement("div");
+                alertElement.className = "alert alert-success alert-dismissible fade show";
+                alertElement.role = "alert";
+                alertElement.innerHTML = "Inicio de sesión exitoso.";
+    
+                const closeButton = document.createElement("button");
+                closeButton.type = "button";
+                closeButton.className = "btn-close";
+                closeButton.setAttribute("data-bs-dismiss", "alert");
+                closeButton.setAttribute("aria-label", "Close");
+                alertElement.appendChild(closeButton);
+    
+                document.getElementById("alertContainer").appendChild(alertElement);
+    
+                setTimeout(() => {
+                    window.location.href = "index.html";
+                }, 1000);
+            } else {
+                const alertElement = document.createElement("div");
+                alertElement.className = "alert alert-danger alert-dismissible fade show";
+                alertElement.role = "alert";
+                alertElement.innerHTML = "Usuario o contraseña incorrecta.";
+                document.getElementById("alertContainer").appendChild(alertElement);
+
+                const closeButton = document.createElement("button");
+                closeButton.type = "button";
+                closeButton.className = "btn-close";
+                closeButton.setAttribute("data-bs-dismiss", "alert");
+                closeButton.setAttribute("aria-label", "Close");
+                alertElement.appendChild(closeButton);
+            }
+        }
+    });
+    
+document.addEventListener("click", (e) => {
+    if (e.target === $reserva) {
+        if ($fechai.value !== "" && $fechaf.value !== "") {
+            e.preventDefault();
+            const alertElement = document.createElement("div");
+            alertElement.className = "alert alert-success alert-dismissible fade show";
+            alertElement.role = "alert";
+            alertElement.innerHTML = "Reserva realizada correctamente.";
+
+            const closeButton = document.createElement("button");
+            closeButton.type = "button";
+            closeButton.className = "btn-close";
+            closeButton.setAttribute("data-bs-dismiss", "alert");
+            closeButton.setAttribute("aria-label", "Close");
+            alertElement.appendChild(closeButton);
+
+            document.getElementById("alertContainer").appendChild(alertElement);
+
+            setTimeout(() => {
+                window.location.href = "index.html";
+            }, 2000); 
+        } else {
+            const alertElement = document.createElement("div");
+            alertElement.className = "alert alert-danger";
+            alertElement.role = "alert";
+            alertElement.innerHTML = "Por favor, diligencia los campos de Fecha inicio y Fecha fin.";
+            document.getElementById("alertContainer").appendChild(alertElement);
         }
     }
 });
 
-document.addEventListener("click", (e)=>{
-    if(e.target === $reserva){
-        if($fechai.value !== "" && $fechaf.value !== ""){
+document.addEventListener("click", (e) => {
+    if (e.target === $contacto) {
+        if ($nombre.value !== "" && $correo.value !== "") {
             e.preventDefault();
-            window.location.href = "index.html";
-        }else{
-        alert("Por favor diligencia los campos de Fecha inicio y Fecha fin.");
+            
+            const alertElement = document.createElement("div");
+            alertElement.className = "alert alert-success alert-dismissible fade show";
+            alertElement.role = "alert";
+            alertElement.innerHTML = "Gracias por contactarnos, pronto nos comunicaremos contigo.";
+
+            const closeButton = document.createElement("button");
+            closeButton.type = "button";
+            closeButton.className = "btn-close";
+            closeButton.setAttribute("data-bs-dismiss", "alert");
+            closeButton.setAttribute("aria-label", "Close");
+            alertElement.appendChild(closeButton);
+
+            document.getElementById("alertContainer").appendChild(alertElement);
+
+            setTimeout(() => {
+                window.location.href = "contacto.html";
+            }, 5000); 
+        } else {
+            const alertElement = document.createElement("div");
+            alertElement.className = "alert alert-danger alert-dismissible fade show";
+            alertElement.role = "alert";
+            alertElement.innerHTML = "Diligencia los campos para enviar el mensaje.";
+            document.getElementById("alertContainer").appendChild(alertElement);
+
+            const closeButton = document.createElement("button");
+            closeButton.type = "button";
+            closeButton.className = "btn-close";
+            closeButton.setAttribute("data-bs-dismiss", "alert");
+            closeButton.setAttribute("aria-label", "Close");
+            alertElement.appendChild(closeButton);
         }
     }
 });
